e6bef5aad314b45d9671366313899dfd6f12a64e
zhi168168 <zhihaosong168@gmail.com>
2026-07-13 18:34:00 +0800

    Add icon to site logo

--- packages/aigotools/src/components/common/logo.tsx
+++ packages/aigotools/src/components/common/logo.tsx
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import Image from "next/image";
 import React from "react";
 import { Istok_Web } from "next/font/google";
 
@@ -12,7 +13,15 @@ const istokWeb = Istok_Web({
 
 export default function Logo({ className }: { className?: string }) {
   return (
-    <Link href={"/"}>
+    <Link className="inline-flex items-center gap-2" href={"/"}>
+      <Image
+        priority
+        alt=""
+        className="h-7 w-7 sm:h-9 sm:w-9 rounded-md"
+        height={36}
+        src="/icon.png"
+        width={36}
+      />
       <h1
         className={clsx(
           "text-primary-800 font-bold text-2xl sm:text-4xl leading-none",
